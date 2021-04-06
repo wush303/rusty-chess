35353998afd8f8721003f114be58841280d6b317
frisbro808 <feri@tv2.dk>
2021-04-06 18:00:33 +0200

    fix loser declared winner bug

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -28,10 +28,10 @@ async fn main() {
     let routes = index.or(game_filter);
     
 
-    warp::serve(routes)
-        .run(([127, 0, 0, 1], 3030))
-        .await;
     //warp::serve(routes)
-    //    .run(([0, 0, 0, 0], 10000))
+    //    .run(([127, 0, 0, 1], 3030))
     //    .await;
+    warp::serve(routes)
+        .run(([0, 0, 0, 0], 10000))
+        .await;
 }
